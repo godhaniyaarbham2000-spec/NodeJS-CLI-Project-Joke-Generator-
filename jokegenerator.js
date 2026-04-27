fae5419da70868c9d1d30a2d12f22cfb970ace06
godhaniyaarbham2000-spec <godhaniyaarbham2000@gmail.com>
2026-04-27 17:31:28 +0530

    Joke Generator clean setup

---
+++ jokegenerator.js
@@ -0,0 +1,27 @@
+// node_modules in mini_project is for this project beacuse we use chalk ===> npm i chalk
+
+import https from "https";
+import chalk from "chalk";
+
+const getJoke = () =>{
+  const url = "https://official-joke-api.appspot.com/random_joke";
+
+  https.get(url, (response)=>{
+    let data = "";
+    response.on("data" , (chunk)=>{
+        data +=chunk
+    });
+    response.on("end",()=>{
+        const joke = JSON.parse(data);
+       //  console.log(joke);
+        console.log(`Here is a random ${joke.type} jokes:`);
+        console.log(chalk.red(`${joke.setup}`));
+        console.log(chalk.blue.bgRed.bold(`${joke.punchline}`));
+    })
+    response.on("error", (err)=>{
+        console.log("Eroor fetching the joke, ${err.message");
+    })
+  })
+}
+
+getJoke();
